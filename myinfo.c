3174bb975333558301d83e102b3ee2fb4ed280b4
hyunjunghun <dnel8332@naver.com>
2025-04-12 15:35:22 +0900

    Update myinfo.c

--- myinfo.c
+++ myinfo.c
@@ -1,6 +1,59 @@
-﻿#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 #include "common.h"
+#include "login.h"
+
+char* get_myinfo_canonical_command(char* input) {
+	struct {
+		const char* synonyms[20];
+		const char* canonical;
+	} myinfo_table[] = {
+		{ { "withdraw", "withdra", "withdr", "withd", "with", "wit", "wi", "w"}, "withdraw"},
+		{ { "change", "chang", "chan", "cha", "ch", "c"}, "change"},
+		{ { "manage", "manag", "mana", "man", "ma", "m"}, "manage"}
+	};
+
+	for (int i = 0; i < sizeof(myinfo_table) / sizeof(myinfo_table[0]); i++) {
+		for (int j = 0; myinfo_table[i].synonyms[j]; j++) {
+			if (strcmp(input, myinfo_table[i].synonyms[j]) == 0)
+				return (char*)myinfo_table[i].canonical;
+		}
+	}
+	return NULL;
+}
 
 void run_myinfo() {
-    printf("run_myinfo() 실행됨 — 회원정보 출력 기능 구현 예정\n");
+	if (is_logged_in == 1) {
+		printf("[My Information]\n");
+		printf("Name: %s\n", User.name);
+		printf("ID: %s\n", User.studentID);
+
+		char input[100];
+
+		while (1) {
+			printf("BookPort: My info - Enter command>");
+			if(!fgets(input, sizeof(input), stdin)) break;
+			trim(input);
+
+			char* cmd = get_myinfo_canonical_command(input);
+			if (!cmd) {
+				printf(".!! Error: Wrong command entered\n");
+				continue;
+			}
+
+			if (strcmp(cmd, "withdraw") == 0) {
+				run_withdraw();
+			}
+			else if (strcmp(cmd, "change") == 0) {
+				run_change();
+			}
+			else if(strcmp(cmd, "manage") == 0) {
+				run_manage();
+			}
+		}
+	}
+	else {
+		run_login();
+	}
+	return;
 }
