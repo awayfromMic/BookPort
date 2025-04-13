b346f23ee94f2c8c14790337498b937d14baca85
hyunjunghun <dnel8332@naver.com>
2025-04-13 15:08:54 +0900

    Update myinfo.c

--- myinfo.c
+++ myinfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "common.h"
 #include "login.h"
 
@@ -22,11 +23,104 @@ char* get_myinfo_canonical_command(char* input) {
 	return NULL;
 }
 
+char* get_manage_canonical_command(char* input) {
+	struct {
+		const char* synonyms[20];
+		const char* canonical;
+	} manage_table[] = {
+		{ { "list", "lis", "li", "l"}, "list"},
+		{ { "record", "recor", "reco", "rec", "re", "r"}, "record"}
+	};
+
+	for (int i = 0; i < sizeof(manage_table) / sizeof(manage_table[0]); i++) {
+		for (int j = 0; manage_table[i].synonyms[j]; j++) {
+			if (strcmp(input, manage_table[i].synonyms[j]) == 0)
+				return (char*)manage_table[i].canonical;
+		}
+	}
+	return NULL;
+}
+
+typedef struct {
+	char date[9];
+	char type[8];
+	char bid[MAX_BID];
+} RecordEntry;
+
+int compare_record_entry(const void* a, const void* b) {
+	return strcmp(((RecordEntry*)a)->date, ((RecordEntry*)b)->date);
+}
+
+void run_record() {
+	RecordEntry* user_record = (RecordEntry*)malloc(2 * lend_return_count * sizeof(RecordEntry)); //각 파일 lincount필요(lend_return_count, bookcount)
+	int user_record_count = 0;
+	for (int i = 0; i < lend_return_count; i++) {
+		if (strcmp(current_user.studentId, Lend_Return[i].userid) == 0) {	//current_user 구조체 필요
+			strcpy(user_record[user_record_count].date, Lend_Return[i].borrowDate);
+			strcpy(user_record[user_record_count].type, "[borrow]");
+			strcpy(user_record[user_record_count].bid, Lend_Return[i].bookBid);
+			user_record_count++;
+			if (Lend_Return[i].returnDate[0] != '\0') {
+				strcpy(user_record[user_record_count].date, Lend_Return[i].returnDate);
+				strcpy(user_record[user_record_count].type, "[return]");
+				strcpy(user_record[user_record_count].bid, Lend_Return[i].bookBid);
+				user_record_count++;
+			}
+		}
+	}
+	user_record = (RecordEntry*)realloc(user_record, user_record_count * sizeof(RecordEntry));
+	qsort(user_record, user_record_count, sizeof(RecordEntry), compare_record_entry);
+	for (int i = 0; i < user_record_count; i++) {
+		char* d = user_record[i].date;
+		printf("%s %s %c%c/%c%c/%c%c\n",
+			user_record[i].type,
+			user_record[i].bid,
+			d[2], d[3], d[4], d[5], d[6], d[7]);
+	}
+	free(user_record);
+}
+
+void run_manage() {
+	char input[100];
+
+	while (1) {
+		printf("BookPort: My info - Enter command>");
+		if (!fgets(input, sizeof(input), stdin)) break;
+		trim(input);
+
+		char* cmd = get_manage_canonical_command(input);
+		if (!cmd) {
+			printf(".!! Error: Wrong command entered\n");
+			continue;
+		}
+
+		if (strcmp(cmd, "list") == 0) {
+			for (int i = 0; i < 5; i++) {
+				if (strlen(current_user.lentBids[i]) == 0) continue;
+
+				for (int j = 0; j < bookcount; j++) {
+					if (strcmp(Book[j].bookBid, current_user.lentBids[i]) == 0) {
+						printf("Title: %s\n", Book[j].title);
+						printf("Author: %s\n", Book[j].author);
+						printf("BID: %s\n", Book[j].bid);
+					}
+				}
+			}
+			break;
+		}
+		else if (strcmp(cmd, "record") == 0) {
+			run_record();
+			break;
+		}
+	}
+	return;
+}
+
 void run_myinfo() {
 	if (is_logged_in == 1) {
 		printf("[My Information]\n");
-		printf("Name: %s\n", User.name);
-		printf("ID: %s\n", User.studentID);
+		printf("Name: %s\n", current_user.name);
+		printf("ID: %s\n", current_user.studentID);
 
 		char input[100];
 
